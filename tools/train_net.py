fa1199d39789d12230c1d7cd68e296b91c59cd53
AgatheMo <60713294+AgatheMo@users.noreply.github.com>
2020-12-02 13:20:25 -0500

    Update train_net.py

--- tools/train_net.py
+++ tools/train_net.py
@@ -162,3 +162,9 @@ if __name__ == "__main__":
         dist_url=args.dist_url,
         args=(args,),
     )
+    from detectron2.data import DatasetCatalog, MetadataCatalog
+    from detectron2.data.datasets import register_coco_instances
+    register_coco_instances("COCO_test_vertebre", {}, "./vertebre_data/datasets/coco/annotations/test_segmentation.json", "./vertebre_data/datasets/coco/images")
+    register_coco_instances("COCO_train_vertebre", {}, "./vertebre_data/datasets/coco/annotations/train_segmentation.json", "./vertebre_data/datasets/coco/images")
+
+
