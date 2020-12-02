ff702e7edbbe7f278dcfb7f9b51b3c29bffbbb78
AgatheMo <60713294+AgatheMo@users.noreply.github.com>
2020-12-02 12:19:59 -0500

    Update register_dataset.py

--- register_dataset.py
+++ register_dataset.py
@@ -1,3 +1,6 @@
+from detectron2.data import DatasetCatalog, MetadataCatalog
 from detectron2.data.datasets import register_coco_instances
-register_coco_instances("COCO_test_vertebre", {}, "./vertebre_data/datasets/coco/annotations/test_segmentation.json", "./vertebre_data/datasets/coco/images")
-register_coco_instances("COCO_train_vertebre", {}, "./vertebre_data/datasets/coco/annotations/train_segmentation.json", "./vertebre_data/datasets/coco/images")
+register_coco_instances("COCO_test_vertebre", {}, "vertebre_data/datasets/coco/annotations/test_segmentation.json", "vertebre_data/datasets/coco/images")
+register_coco_instances("COCO_train_vertebre", {}, "vertebre_data/datasets/coco/annotations/train_segmentation.json", "vertebre_data/datasets/coco/images")
+COCO_vertebre_train = MetadataCatalog.get("COCO_train_vertebre")
+COCO_vertebre_test = MetadataCatalog.get("COCO_test_vertebre")
